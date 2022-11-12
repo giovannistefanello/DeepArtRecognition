008a5dd5191c4202f62876a18e540038e39b54bf
Giovanni Stefanello <giovannistefanello@gmail.com>
2022-11-12 20:36:28 +0100

    Added tf.data.Dataset pipelines

--- src/data/data_loading.py
+++ src/data/data_loading.py
@@ -1,50 +1,85 @@
 # FUNCTIONS AND UTILITIES FOR DATA LOADING
-
-# standard libraries
-import glob
 import logging
 import os
 
+import matplotlib.pyplot as plt
 # third party libraries
 import pandas as pd
+import tensorflow as tf
 
 # set logger
-logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(name)s][%(levelname)s]: %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s[%(name)s][%(levelname)s]: %(message)s')
 logger = logging.getLogger(__name__)
 
-
-def check_ascii_conformity(df: pd.DataFrame):
-
-    def check_one_col(column):
-        # check for weird characters in artist name
-        weird_entries = set()
-        for entry in column:
-            if any([not 0 <= ord(c) <= 127 for c in entry]):
-                weird_entries.add(entry)
-        return weird_entries
-
-    for col in df.columns:
-        weird_names = check_one_col(df[col])
-        if weird_names:
-            logger.warning(f'Entries {weird_names} in column \'{col}\' contain some non-ascii characters.')
+# global variables
+AUTOTUNE = tf.data.AUTOTUNE
 
 
-def create_dataframe(data_dir):
+def decode_image(filepath, img_size):
+    image = tf.io.decode_jpeg(tf.io.read_file(filepath))
+    # TODO: THINK ABOUT A BETTER WAY TO RESIZE
+    # TODO: VARIOUS CROPPINGS: HOW?
+    if img_size:
+        image = tf.image.resize(image, img_size)  # resize to uniform size
+    image = image/255.  # rescale image in [0,1]
+    return image
 
-    # basic data fetch
-    image_filenames = glob.glob(os.path.join(data_dir, 'images/**/*.jpg'),
-                                recursive=True)
-    # create a bare data info df
-    df = pd.DataFrame({'filepath': image_filenames})
-    df['artist'] = df['filepath'].apply(lambda x: x.split(os.path.sep)[-2].replace('_', ' '))
 
-    logger.info(f'Created dataframe ({len(df)} rows) from data found in {data_dir}.')
+def input_pipeline(dataframe: pd.DataFrame, img_size=None):
 
-    return df
+    num_classes = len(dataframe['target'].unique())
+
+    def process_row(row):
+        label_oh = tf.one_hot(row['target'],
+                              depth=num_classes)
+        image = decode_image(row['filepath'], img_size=img_size)
+
+        # there are some weird grayscale images (H,W,None) or (H,W,1)
+        if img_size:
+            if tf.shape(image)[-1] != 3:
+                if tf.rank(image) == 2:
+                    image = tf.stack([image] * 3, axis=2)
+                elif tf.shape(image)[-1] == 1:
+                    image = tf.repeat(image, 3, axis=2)
+                image = tf.reshape(image, (*img_size, 3), )
+
+        return image, label_oh
+
+    ds = tf.data.Dataset.from_tensor_slices(dict(dataframe))
+    ds = ds.map(process_row, num_parallel_calls=AUTOTUNE)
+    return ds
+
+
+def performance_pipeline(ds: tf.data.Dataset, batchsize=0, bufsiz=0, cache=False, cache_path: str = ""):
+    if cache:
+        ds = ds.cache(cache_path)
+    if bufsiz > 0:
+        ds = ds.shuffle(buffer_size=bufsiz)
+    if batchsize > 0:
+        ds = ds.batch(batchsize)
+    ds = ds.prefetch(buffer_size=AUTOTUNE)
+    return ds
 
 
 if __name__ == '__main__':
+    from src.data import df_utils
+
     DATA_DIR = '../../data'
-    df = create_dataframe(DATA_DIR)
-    check_ascii_conformity(df)
-    print(df.head())
+
+    img_dir = os.path.join(DATA_DIR, 'images')
+    df = df_utils.create_dataframe(img_dir)
+
+    # add dummy target label
+    df['target'] = [i % 7 for i in range(len(df))]
+
+    # apply pipelines
+    ds = input_pipeline(df, img_size=(256, 256))
+    ds = performance_pipeline(ds, batchsize=16, bufsiz=16)
+
+    print('Checking one element in dataset')
+    for elem in ds.take(1):
+        print('data_shape:', elem[0].shape)
+        print('target_shape:', elem[1].shape)
+        plt.imshow(elem[0][0])
+        plt.title(f'target: {elem[1][0]}')
+        plt.show()
